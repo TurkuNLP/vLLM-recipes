495ef7290d5d7489295ef9ae490438a895ec12f9
Gelato1337 <84318227+Gelato1337@users.noreply.github.com>
2024-11-15 10:38:39 +0200

    Add files via upload

---
+++ vLLM_Lumi_Python/runSlurm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=llamaRun
+#SBATCH --account=*input your project here*
+#SBATCH --partition=small-g
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=15
+#SBATCH --gpus-per-node=8
+#SBATCH --mem=250G
+#SBATCH --time=1:00:00
+
+module purge
+module use /appl/local/csc/modulefiles
+module load pytorch
+
+
+gpu-energy --save
+
+srun python3 vLLM_inference.py
+
+gpu-energy --diff
